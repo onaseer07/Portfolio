04bf23a8146a189b32b4d2ee969b44eb24f6b746
onaseer07 <onaseer07@gmail.com>
2018-09-04 23:02:57 -0500

    added scrollmagic

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,4 +2,35 @@ $(document).ready(function(){
         //Backstretch.js
         $.backstretch('assets/images/bg.jpg');
         
+        //scrollMagic.js
+            //$(window).on("resize", function(e) {
+                if ($(window).width() >= 768) {
+                        let controller = new ScrollMagic.Controller();
+                
+                        let logo = new ScrollMagic.Scene({
+                            triggerElement: '#introduction',
+                            triggerHook: 1,
+                            reverse: false,
+                        })
+                            .setClassToggle('#introduction','fade-in')
+                            .addTo(controller);
+                        let aboutMe = new ScrollMagic.Scene({
+                            triggerElement: '.aboutMe',
+                            triggerHook: 1,
+                            reverse: true,
+                        })
+                            .setClassToggle('#aboutMe','fade-in')
+                            .addTo(controller);
+                        let menu = new ScrollMagic.Scene({
+                            triggerElement: '#menuContent',
+                            triggerHook: 1,
+                            reverse: false,
+                        })
+                            .setClassToggle('#menu','fade-in')
+                            .addTo(controller);
+                    }else if ($(window).width() < 768) {
+                        $('#intro').css('opacity','1');
+                        $('#aboutUs').css('opacity', '1');
+                        $('#menu').css('opacity', '1');
+                    }
 });
